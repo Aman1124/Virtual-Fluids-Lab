8e7a7766355920ae2784172416ef6d9ff5281b69
Aman1124 <amankumarthakur666@gmail.com>
2020-08-10 21:29:45 +0530

    Bernoulli Setup Added

---
+++ app/src/main/java/com/example/virtualfluidlab/Bernoulli.java
@@ -0,0 +1,14 @@
+package com.example.virtualfluidlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bernoulli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bernoulli);
+    }
+}
--- app/src/main/java/com/example/virtualfluidlab/ListView.java
+++ app/src/main/java/com/example/virtualfluidlab/ListView.java
@@ -4,12 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ListView extends AppCompatActivity {
+
+    Button bernoulli;
+    Button VNotch;
+    Button MetaCenter;
+
+    public void switchToBernoulli(View view){
+        Intent intent = new Intent(getApplicationContext(),Bernoulli.class);
+        startActivity(intent);
+    }
+
+    public void switchToVnotch(View view){
+
+    }
+
+    public void switchToMetacenter(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        bernoulli = findViewById(R.id.bernoulli);
+        VNotch = findViewById(R.id.vnotch);
+        MetaCenter = findViewById(R.id.metacenter);
+
     }
 }
